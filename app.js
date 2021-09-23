6fd27c882749b1b83e1299373ab1476dba85ebe4
Vitusan <0vetor0@gmail.com>
2021-09-22 21:27:22 -0300

    função create card feita.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ document.querySelector('#data-criacao').valueAsDate = new Date();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    criarCard();
+    criarCardUser();
+    criarCardAPI();
 });
 
 form.addEventListener('input', (e) => {
@@ -31,14 +32,94 @@ descInput.addEventListener('invalid', () => {
     }
 });
 
-let criarCard = function () {
-    // TO DO: Criar card
+let criarCardUser = function () {
+    // TO DO: Refatorar função para página card.html (remover cardSection e Append)
     // USAR LOCALSTORAGE
-    let cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
-    let cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    
+
+    // coluna de cards
+    let cardSection = document.createElement('div');
+    cardSection.classList.add('col-md-3', 'col-sm-12')
+
+    // card 
+    let card = document.createElement('div');
+    card.classList.add('card', 'container');
+
+    // row de elementos do card
+    let cardRow = document.createElement('div');
+    cardRow.classList.add('row','justify-content-center');
+
+    // coluna de elementos do card
+    let cardCol = document.createElement('div');
+    cardCol.classList.add('col-9');
+
+    // row de datas
+    let dataRow = document.createElement('div');
+    dataRow.classList.add('row','justify-content-around');
+
+    // coluna da data de criação
+    let dataInicioCol = document.createElement('div');
+    dataInicioCol.classList.add('col-sm-auto');
+
+    // coluna da data de conclusão
+    let dataConclusaoCol = document.createElement('div');
+    dataConclusaoCol.classList.add('col-sm-auto');
+
+    // row de botões
+    let btnRow = document.createElement('div');
+    btnRow.classList.add('row','justify-content-around');
+
+    // coluna do checkbox de conclusão
+    let checkCol = document.createElement('div');
+    checkCol.classList.add('col-sm-auto');
+
+    // coluna do botão de exclusão
+    let btnExcluirCol = document.createElement('div');
+    btnExcluirCol.classList.add('col-sm-auto');
+
+    // elementos do card
+    let cardDesc = document.createElement('div');
+    cardDesc.classList.add('container','card-desc');
+    cardDesc.innerText = descInput.value;
+    let cardData = document.createElement('div');
+    cardData.classList.add('container','card-data');
+    cardData.innerText = document.querySelector('#data-criacao').value;
+    let cardFinal = document.createElement('div');
+    cardFinal.classList.add('container','card-final');
+    cardFinal.innerText = document.querySelector('#data-conclusao').value;
+    let cardCheck = document.createElement('div');
+    cardCheck.classList.add('form-check');
+    let cardCheckInput = document.createElement('input');
+    cardCheckInput.classList.add('form-check-input');
+    cardCheckInput.type = 'checkbox';
+    cardCheckInput.id = 'checkbox';
+    let cardCheckLabel = document.createElement('label');
+    cardCheckLabel.classList.add('form-check-label');
+    cardCheckLabel.setAttribute('for', 'checkbox');
+    cardCheckLabel.innerText = 'Concluído';
+    let cardBtnExcluir = document.createElement('button');
+    cardBtnExcluir.classList.add('btn','btn-danger','btn-excluir');
+    cardBtnExcluir.innerText = 'Excluir';
+
+    // adicionando elementos ao card
+    card.appendChild(cardRow);
+    cardRow.appendChild(cardCol);
+    cardCol.appendChild(cardDesc);
+    cardCol.appendChild(dataRow);
+    dataRow.appendChild(dataInicioCol);
+    dataRow.appendChild(dataConclusaoCol);
+    dataInicioCol.appendChild(cardData);
+    dataConclusaoCol.appendChild(cardFinal);
+    cardCol.appendChild(btnRow);
+    btnRow.appendChild(checkCol);
+    checkCol.appendChild(cardCheck);
+    cardCheck.appendChild(cardCheckInput);
+    cardCheck.appendChild(cardCheckLabel);
+    btnRow.appendChild(btnExcluirCol);
+    btnExcluirCol.appendChild(cardBtnExcluir);
+
+    // adicionando card ao cardCol
+    cardSection.appendChild(card);
+    document.body.appendChild(cardSection);
 }
 
 let checkDateRange = function (input) {
