585a8a231b7c8d11adecf31b9c60e65416f3c2d4
Bruno <brb.dev@outlook.com>
2021-09-27 20:01:43 -0400

    altera funcao deletar tarefa

--- js/cardCreator.js
+++ js/cardCreator.js
@@ -73,22 +73,24 @@ export function criarCardUser (titulo,descricao,dataInicio,dataFinal,concluido) 
         cardData.style.textDecoration="initial"
         }
     }
-    if(concluido===true){
-        cardCheckInput.checked = true;
-        cardTitle.classList.remove('fw-bold');
-    }
+    // if(concluido===true){
+    //     cardCheckInput.checked = true;
+    //     cardTitle.classList.remove('fw-bold');
+    // }
+
     let cardBtnExcluir = document.createElement('button');
     cardBtnExcluir.classList.add('btn','btn-danger','btn-excluir');
     cardBtnExcluir.innerText = 'Excluir';
-    
-    cardBtnExcluir.onclick = function(){
+       
+    btnExcluirCol.addEventListener('click', (e) => {
+        cardBtnExcluir = e.target
         let delConfirm = confirm("Deseja excluir a nota?")
         if (delConfirm){
         card.parentNode.removeChild(card)
         localStorage.removeItem('card')
         localStorage.removeItem('cardUser')
         }
-    }  
+    })
 
     // adicionando elementos ao card
     card.appendChild(cardRow);
